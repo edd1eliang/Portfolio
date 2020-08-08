619262651f79b9ce7c290f39db38b01c345933b8
Eddie Liang <eddieliang@Eddies-MacBook-Pro-2.local>
2020-08-08 14:19:39 -0400

    git commit

--- src/editable-stuff/blog.jsx
+++ src/editable-stuff/blog.jsx
@@ -1,39 +1,39 @@
 import { BlogBuilder } from "../components/blog/BlogBuilder";
 const bloglist = [];
-
+    
 const blog = new BlogBuilder({
   title: "My First Blog",
   image: "img",
   description:
-    "Description Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, dolores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia provident similique corporis rem ullam odio excepturi voluptas harum, architecto sit!",
+    "Test",
 })
   .addHeading("Heading")
   .addParagraph(
-    "1st Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vero reiciendis quas, autem sit culpa? Quibusdam, cupiditate voluptate a non nulla aliquid enim doloremque ullam, facilis quisquam similique hic omnis!"
+    "Test1"
   )
   .addParagraph(
-    "2nd Paragraph Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ad, temporibus quae obcaecati eum expedita pariatur aspernatur recusandae beatae iste soluta sunt blanditiis dolore ipsam quia laboriosam quas perspiciatis architecto?"
+    "Test3"
   );
 
 const blog1 = new BlogBuilder({
   title: "My Second Blog",
   image: "img",
   description:
-    "Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore aut neque consectetur voluptatem quam nihil, facere earum adipisci, animi beatae dicta maiores, ipsam delectus ab molestias dolorum aperiam? Sapiente?",
+    "Test1",
 })
   .addHeading("Heading")
   .addParagraph(
-    "1st Paragraph Aspernatur vero reiciendis quas, autem sit culpa? Quibusdam, cupiditate voluptate a non nulla aliquid enim doloremque ullam, facilis quisquam similique hic omnis!"
+    "Test2"
   )
   .addParagraph(
-    "2nd Paragraph Consequuntur ad, temporibus quae obcaecati eum expedita pariatur aspernatur recusandae beatae iste soluta sunt blanditiis dolore ipsam quia laboriosam quas perspiciatis architecto?"
+    "TEst3"
   )
   .addHeading("New Heading")
   .addParagraph(
-    "1st Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vero reiciendis quas, autem sit culpa? Quibusdam, cupiditate voluptate a non nulla aliquid enim doloremque ullam, facilis quisquam similique hic omnis!"
+    "Test4"
   )
   .addParagraph(
-    "2nd Paragraph Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ad, temporibus quae obcaecati eum expedita pariatur aspernatur recusandae beatae iste soluta sunt blanditiis dolore ipsam quia laboriosam quas perspiciatis architecto?"
+    "Tes5"
   );
 
 bloglist.push(blog1);
